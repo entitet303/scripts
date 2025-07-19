0ff5174b14e16878746bcdfbbb8596adb349b0d1
entitet303 <92458781+entitet303@users.noreply.github.com>
2025-07-19 21:18:02 +0200

    Update mount_share.sh

--- mount_share.sh
+++ mount_share.sh
@@ -1,46 +1,29 @@
 #!/bin/bash
 
-set -e
+# Benutzer-Eingaben
+read -p "Geben Sie die CIFS-Freigabe ein (z.B. //192.168.2.50/jellyfin): " SHARE
+read -p "Geben Sie den Mount-Punkt ein (z.B. /data): " MOUNT_POINT
+read -p "Geben Sie den Benutzernamen ein: " USERNAME
+read -s -p "Geben Sie das Passwort ein: " PASSWORD
+echo ""
 
-# Sicherstellen, dass das Skript als root läuft
-if [ "$EUID" -ne 0 ]; then
-    echo "Bitte als root ausführen (sudo)."
-    exit 1
-fi
+# Pakete installieren
+apt update && apt install -y cifs-utils
 
-# Benötigte Pakete installieren
-apt update
-apt install -y cifs-utils nfs-common
+# Anmeldeinformationen speichern
+CREDENTIALS_FILE="/root/.cifs_credentials"
+echo "username=$USERNAME" > $CREDENTIALS_FILE
+echo "password=$PASSWORD" >> $CREDENTIALS_FILE
+chmod 600 $CREDENTIALS_FILE
 
-if [[ "$1" == "undo" ]]; then
-    read -p "Mount-Punkt der entfernt werden soll: " MOUNT_POINT
-    sed -i \"\\#$MOUNT_POINT#d\" /etc/fstab
-    umount "$MOUNT_POINT" 2>/dev/null || true
-    echo "Eintrag entfernt."
-    exit 0
-fi
+# Mount-Punkt erstellen
+mkdir -p $MOUNT_POINT
 
-read -p "Typ der Freigabe (smb/nfs): " TYPE
-read -p "Freigabe-URL (z.B. //server/share oder 192.168.1.1:/path): " SHARE
-read -p "Mount-Punkt (z.B. /mnt/share): " MOUNT_POINT
-
-if [[ "$TYPE" == "smb" ]]; then
-    read -p "Benutzername: " USERNAME
-    read -s -p "Passwort: " PASSWORD
-    echo
-    CREDENTIALS_FILE="/root/.${MOUNT_POINT##*/}_cred"
-    echo "username=$USERNAME" > "$CREDENTIALS_FILE"
-    echo "password=$PASSWORD" >> "$CREDENTIALS_FILE"
-    FSTAB_ENTRY="$SHARE $MOUNT_POINT cifs credentials=$CREDENTIALS_FILE,iocharset=utf8,file_mode=0777,dir_mode=0777,nofail 0 0"
-elif [[ "$TYPE" == "nfs" ]]; then
-    FSTAB_ENTRY="$SHARE $MOUNT_POINT nfs defaults,nofail 0 0"
-else
-    echo "Unbekannter Typ: $TYPE"
-    exit 1
-fi
-
-mkdir -p "$MOUNT_POINT"
+# fstab-Eintrag hinzufügen
+FSTAB_ENTRY="$SHARE $MOUNT_POINT cifs credentials=$CREDENTIALS_FILE,iocharset=utf8,file_mode=0777,dir_mode=0777,nofail 0 0"
 grep -qxF "$FSTAB_ENTRY" /etc/fstab || echo "$FSTAB_ENTRY" >> /etc/fstab
+
+# Mount ausführen
 mount -a
 
-echo "Freigabe wurde erfolgreich gemountet." 
+echo "CIFS-Freigabe wurde erfolgreich gemountet!"
